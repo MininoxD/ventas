a2642a4fd6de2400dc71db75952ade4562be9df6
Lau Prado De L <arual_15@hotmail.es>
2020-03-16 19:40:03 -0500

    modif 2

---
+++ includes/admin.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/style.css">
+    <style>
+        .hh:hover {
+            transform: scale(1.1);
+            z-index:1;
+transition: all 1s ease;
+    box-shadow: 0 12px 16px rgba(0, 0, 0, 0.5);
+}
+.ff{
+    font-size: 80px;
+    text:center;
+}
+.s{
+    font-size: 100px;
+    text:center;
+}
+    </style>
+    <title>Venta</title>
+</head>
+<body class="ven" style="overflow-y:auto; position: relative;" >
+
+    <div class="row mt-3" >
+            <div class="col-10 d-flex justify-content-end ">
+                <p >Bienvenido</p>  
+                <a href="includes/logaut.php"><img src="img/login.png" alt=""></a>   
+            </div>
+    </div>
+
+    <div class="row" style="height:150px"; >
+            <div class="col-12 d-flex justify-content-center ">
+                <p ><h1 class="display-2 font-italic ">CATEGORIAS</h1></p>   
+            </div>
+    </div>
+   
+    <div class="container-fluid d-flex justify-content-center">
+                <div class="card-group">
+                        <div class="card hh" style="background-color:#FFFF61;">
+                            <img src="../img/utilesxd.png" alt="...">
+                            <div class="card-body">
+                            </div>
+                            <h3><p class="text-center"> UTILES DE ESCRITORIO </p></h3>
+                        </div>
+                        <div class="card hh" style="background-color:#84FBE7;">
+                            <img src="../img/limpio2.png" class="card-img-top">
+                            <div class="card-body">
+                            </div>
+                            <h3><p class="text-center">MATERIALES DE LIMPIEZA </p></h3>
+                        </div>
+                        <div class="card hh" style="background-color:#E582F8;">
+                            <img src="../img/cocina2.png" class="card-img-top">
+                            <div class="card-body">
+                            </div>
+                            <h3><p class="text-center">IMPLEMENTOS DE COCINA </p></h3>
+                        </div>
+                        <div class ="card hh" style="background-color:#FB93D1;">
+                            <img src="../img/come.png" class="card-img-top" >
+                            <div class="card-body">
+                            </div>
+                            <h3><p class="text-center">ACCESORIOS </p></h3>
+                        </div>
+                </div>           
+    
+    </div>
+</br>
+</br>
+    <div class="container">
+        <div  class="row">
+            <div class="col-5 " style="background-color:#FF6A6A;">
+                <h3><p class="text-center" style="color:#FFFFFF">VENTAS DE D</p></h3>
+            </div>
+             <div class="col-7 " style="background-image:url('../img/verde123.png');background-size:cover; background-repeat:no-repeat; ">
+                <p class="ff text-center" >TOTAL  </p>
+                <p class="s text-center" >TOTAL  </p>
+            </div>
+        </div> 
+    </div> 
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="js/bos.js"></script>
+        <script src="js/lib.js"></script>
+        <script src="js/jquery-3.4.1.js">
+    </script>
+</body>
+</html>
